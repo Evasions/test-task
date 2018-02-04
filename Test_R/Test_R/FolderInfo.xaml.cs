43d2b723d79836fb245c13c23d69018a25c7bcc4
Evasions <mr.sokorev@gmail.com>
2018-02-04 22:56:37 +0200

    First commit

---
+++ Test_R/Test_R/FolderInfo.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test_R
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FolderInfo : ContentPage
+	{
+        //initilaze objects
+        JsonDeserialize jsonDeserialize1;
+        public ObservableCollection<Folder> secondFolderInfo { get; set; }
+        Root secondFolder;
+        public FolderInfo (string _pathFolder)
+		{
+			InitializeComponent ();
+            jsonDeserialize1 = new JsonDeserialize();
+            secondFolderInfo = new ObservableCollection<Folder>();
+            //setup settings listView
+            ListView secondListView = new ListView();
+            secondListView.RowHeight = 60;
+            secondListView.ItemTemplate = new DataTemplate(typeof(Folder));
+            //call ShowFiles method
+            ShowFiles(_pathFolder);
+        }
+
+
+        private void ShowFiles(string _path)
+        {
+            string pathFolder;
+            string[] split;
+            //geting and answer from server and write in the row 
+            string answer = jsonDeserialize1.Deserialize(_path).ToString();
+            // parse answer
+            JObject jobject = JObject.Parse(answer);
+            // geting parametrs from content folder
+            var arrFolder = jobject.SelectToken("content");
+            // geting list folders
+            var listFolder = arrFolder.ToList();
+            secondFolder = JsonConvert.DeserializeObject<Root>(answer);
+            //get folder options
+            for (int i = 0; i < listFolder.Count; i++) {
+                pathFolder = listFolder[i].Path;
+                split = pathFolder.Split('[',']','\'');
+                secondFolder.name = split[2];
+                //setup parametrs folder for display
+                try {
+                    secondFolderInfo.Add(new Folder { name = secondFolder.name, birthtime = secondFolder.birthtime });
+                }
+                catch (Exception) {
+                    labelFolder.Text = "Oops! Something went wrong";
+                }
+                
+            }
+            secondListView.ItemsSource = secondFolderInfo;
+        }
+        private void TextCell_Tapped(object sender, EventArgs e)
+        {
+            TextCell txtCell = (TextCell)sender;
+            //transition to the following view
+            //in the properties we pass the name and path
+            Navigation.PushAsync(new ShowFile(txtCell.Text, secondFolder.path, secondFolder.size, secondFolder.birthtime));
+            
+
+        }
+    }
+}
---
+++ Test_R/Test_R/ShowFile.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test_R
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowFile : ContentPage
+    {
+        public ShowFile(string _name, string _path, int _size, DateTime _dateCreate)
+        {
+            InitializeComponent();
+            ShowFileContent(_name, _path, _size, _dateCreate);
+        }
+
+        private void ShowFileContent(string _name, string _path, int _size, DateTime _dateCreate)
+        {
+            try {
+                labelSize.Text = "Size: " + _size.ToString();
+                labelDateCreator.Text = _dateCreate.Date.ToString();
+                string[] splitTxt = _name.Split('.', '\'');
+                //determine the type of file
+                if (String.Equals(splitTxt[splitTxt.Length - 1], "jpg")) {
+                    imageContent.Source = ImageSource.FromUri(new Uri("https://fileserver-tsh.herokuapp.com/" + _path + "/" + _name));
+                }
+                else { }
+            }
+            catch (Exception) {
+
+                labelSecond.Text = "Oops!Something went wrong";
+            }
+           
+          
+        }
+    }
+}
